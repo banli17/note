3c88ee2d83c16af6c747c5a0871c275ca850983e
banli17 <867889876@qq.com>
2022-05-08 23:16:54 +0800

    update

---
+++ 81-vue-router/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 81-vue-router/src/views/About.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>About content</div>
+</template>
